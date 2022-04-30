89ceb1b0c2ff2b84a2678feca48ad1fbc95c799d
pavel_ishmukov <p-pup1@mail.ru>
2022-05-01 00:09:05 +0300

    added a sliding panel

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import {PATH} from "./Pages";
-
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            <span> <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink> </span>
-            <span> <NavLink to={PATH.JUNIOR}>junior</NavLink> </span>
-            <span> <NavLink to={PATH.JUNIORPLUS}>junior+</NavLink> </span>
+        <div className={s.okno}>
+            <span> <NavLink to={PATH.PRE_JUNIOR} className={s.preJunior}>pre-junior</NavLink> </span>
+            <span> <NavLink to={PATH.JUNIOR} className={s.junior}>junior</NavLink> </span>
+            <span> <NavLink to={PATH.JUNIORPLUS} className={s.juniorplus}>junior+</NavLink> </span>
 
         </div>
     )
